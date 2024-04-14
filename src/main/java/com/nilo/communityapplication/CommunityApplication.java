df8e7673a2f0d03f695d3bd05db019b340096868
niloaydin <nilay.aydn97@gmail.com>
2024-04-15 02:19:09 +0300

    Initial commit

---
+++ src/main/java/com/nilo/communityapplication/CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.nilo.communityapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nilo/communityapplication/service/JwtService.java
@@ -0,0 +1,10 @@
+package com.nilo.communityapplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUserEmail(String token){
+        return null;
+    }
+}
